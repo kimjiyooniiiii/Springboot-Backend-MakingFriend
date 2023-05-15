f63c1138246c124bd9ec5e5e31a1c14c6390c694
star-moon-cloud-k <kspecial44@gmail.com>
2023-05-15 13:31:10 +0900

    FIX : 로그인 오류

--- src/main/java/com/knucapstone/rudoori/token/TokenRepository.java
+++ src/main/java/com/knucapstone/rudoori/token/TokenRepository.java
@@ -1,8 +1,8 @@
 package com.knucapstone.rudoori.token;
 
-import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +13,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 select t from Token t inner join UserInfo u on t.user = u.userId 
 where u.userId = :id and (t.expired = false or t.revoked = false)
 """)
-        List<Token> findAllValidTokenByUser(@PathParam("id") String id);
+        List<Token> findAllValidTokenByUser(@Param("id") String id);
         Optional<Token> findByToken(String token);
 
 }
